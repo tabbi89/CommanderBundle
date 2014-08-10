83ceffb250047409141555c114b7c832712d04d6
Tomasz Lopusiewicz <tomasz.lopusiewicz@joobster.pl>
2014-08-10 14:24:35 +0200

    Remove user specific files from gitingore and fix unsed variables

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tabbi89_commander');
+        $treeBuilder->root('tabbi89_commander');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
